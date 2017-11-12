0101ce06bf7b4e3d7dee25494ce8e97298340b14
brayan salgado <bss1442009@hotmail.com>
2017-11-12 18:09:06 -0500

    Domino

--- src/dominio/Alquiler.java
+++ src/dominio/Alquiler.java
@@ -9,8 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import Modelo.Cabana;
-import Modelo.Titular;
+
 @Entity
 @Table(name="tb_alquiler")
 
